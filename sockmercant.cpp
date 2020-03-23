cf2ffbde7f540210d51533b8334042120bfd4d87
vairavan2298 <39181570+vairavan2298@users.noreply.github.com>
2020-03-23 22:15:41 +0530

    Update sockmercant.cpp

--- sockmercant.cpp
+++ sockmercant.cpp
@@ -6,14 +6,14 @@ int main() {
  int n,repeat[101];
     cin>>n;
  
- for(int i = 0; i < n; i++) {
+ for(int i = 0; i < n; i++) {//increase the count of  numbers
         int c;
         cin >> c;
         repeat[c]++;
     }
 
  int count = 0;
- for(int i = 0; i <= 100; i++){
+ for(int i = 0; i <= 100; i++){ //find the count of numbers that occurs odd number of times
          count += repeat[i] / 2;
      }
  cout << count<< endl;
